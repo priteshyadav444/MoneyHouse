2d0a82afbe483b093941074ca32f0a7d0808b8a6
Pritesh Yadav <priteshyadav2015@Gmail.com>
2022-02-25 10:54:35 +0530

    remove file sharing

--- filesharing/routes/upload.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router();
-const File = require("../models/file")
-
-
-router.get("/", (req, res) => {
-    res.render("upload")
-})
-module.exports = router;
